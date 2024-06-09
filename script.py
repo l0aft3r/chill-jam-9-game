c89fe9e8681855c070535d051e089fc67320c0ef
danielfox43 <danielo.rusnaco@gmail.com>
2024-06-10 01:26:02 +0300

    helow

--- script.py
+++ script.py
@@ -8,14 +8,37 @@ pygame.init()
 size = width, height = 1280, 720
 black = 0, 0, 0
 
-objects = []
 screen = pygame.display.set_mode(size)
-px = 100
-py = 200
-ball = pygame.image.load("png-transparent-football-ball-game-soccer-ball-soccer-ball-artwork-miscellaneous-sport-sports-equipment-thumbnail.png").convert_alpha()
 clock = pygame.time.Clock()
 dt = 0
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.speed = 400
+        self.image = pygame.image.load("chill-jam-9-game\png-transparent-football-ball-game-soccer-ball-soccer-ball-artwork-miscellaneous-sport-sports-equipment-thumbnail.png").convert_alpha()
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+    
+    def update(self, dt):
+         keys = pygame.key.get_pressed()
+         if keys[pygame.K_w]:
+             self.x -= 300 * dt
+         elif keys[pygame.K_s]:
+             self.x += 300 * dt
+         if keys[pygame.K_a]:
+             self.x -= 300 * dt
+         elif keys[pygame.K_d]:
+             self.x += 300 * dt
+    def draw(self):
+        self.rotate = pygame.transform.rotozoom(self.image, 33, 1)
+        self.retartded_rect = self.rotate.get_rect(center = (self.x, self.y))
+        screen.blit(self.rotate, self.retartded_rect)
+       
+player = Player(600, 400)
+objects = pygame.sprite.Group()
+objects.add(player)
 
 while True:
     for event in pygame.event.get():
@@ -23,26 +46,13 @@ while True:
             pygame.quit()
             sys.exit()
     
-    mous_pos = pygame.mouse.get_pos()
+    mouse_pos = pygame.mouse.get_pos()
     screen.fill(black)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        py -= 300 * dt
-    elif keys[pygame.K_s]:
-        py += 300 * dt
-    if keys[pygame.K_a]:
-        px -= 300 * dt
-    elif keys[pygame.K_d]:
-        px += 300 * dt
 
-
-    anglemous = math.degrees(math.atan2(mous_pos[0] - px, mous_pos[1] - py)) 
-
-    rotatedball = pygame.transform.rotozoom(ball, anglemous, 1)
-
-    rectball = rotatedball.get_rect(center = (px, py))
-    
-    screen.blit(rotatedball, rectball)
+    mouse_angle = math.degrees(math.atan2(mouse_pos[0] - player.x, mouse_pos[1] - player.y)) 
+    objects.update(dt)
+    objects.draw(screen)
+   
     
     pygame.display.flip()
 
