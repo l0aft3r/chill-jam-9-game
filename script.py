9922e1f554f693c05f10afa28a4695de60ff3fbd
l0aft3r <l0aft3r@gmail.com>
2024-06-11 15:23:31 +0300

    added cooldown

--- script.py
+++ script.py
@@ -1,16 +1,20 @@
 import sys
 import pygame
 import math
+import time
 
 
 pygame.init()
 
-size = width, height = 1280, 720
+size = width, height = 1280, 600
 black = 0, 0, 0
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) #You can comment this line out and set your own screen dimensions if you find it annoying
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 dt = 0
+
+        
+
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, mouse_angle):
         super().__init__()
@@ -34,12 +38,7 @@ class Bullet(pygame.sprite.Sprite):
             print('deleting')
             pygame.sprite.Sprite.kill(self)
 
-
-
-        
-
     def draw(self):
-        print('e')
         self.rotate = pygame.transform.rotozoom(self.image, self.mouse_angle, 1)
         self.rect = self.rotate.get_rect(center=(self.x, self.y))
         screen.blit(self.rotate, (self.rect.center))
@@ -48,56 +47,84 @@ class Bullet(pygame.sprite.Sprite):
         
 
 class Player(pygame.sprite.Sprite):
-
     def __init__(self, x, y):
         super().__init__()
-        self.temp = 0
         self.x = x
         self.y = y
         self.speed = 400
-        self.image = pygame.image.load("png-transparent-football-ball-game-soccer-ball-soccer-ball-artwork-miscellaneous-sport-sports-equipment-thumbnail.png").convert_alpha()
+        self.health = 100
+        self.sun_bar = 100
+        self.pressed = False
+        self.time = 5
+        self.image = pygame.image.load("player.png").convert_alpha()
         self.rect = self.image.get_rect(center=(self.x, self.y))
-    
+        self.last_fired = time.time()
+        self.fire_cooldown = 1
     def update(self, dt):
+         self.time -= dt
          keys = pygame.key.get_pressed()
          if keys[pygame.K_w]:
-             self.y -= self.speed * dt
+             self.y -= 300 * dt
          elif keys[pygame.K_s]:
-             self.y += self.speed * dt
+             self.y += 300 * dt
          if keys[pygame.K_a]:
-             self.x -= self.speed * dt
+             self.x -= 300 * dt
          elif keys[pygame.K_d]:
              self.x += 300 * dt
+         if True:
+            if pygame.mouse.get_pressed()[0] and time.time() - self.last_fired > self.fire_cooldown:
+                bullet = Bullet(self.x, self.y, mouse_angle)
+                bullets.add(bullet)
+                self.last_fired = time.time()
+                #self.pressed = True
+         
+         #drains the sun bar every 5 seconds
+         if self.time <= 0:
+            print('sun damage')
+            self.sun_bar -=1
+            self.time = 5
+            
+
+         #kill the player when health reaches 0
+         if self.health <= 0 or self.sun_bar <= 0:
+             pygame.sprite.Sprite.kill(self)
+  
+            
     def draw(self, mouse_angle):
-        print('he')
         self.rotate = pygame.transform.rotozoom(self.image, mouse_angle, 1)
         self.orthogonally_tilted_quadrilateral_plane = self.rotate.get_rect(center = (self.x, self.y))
         screen.blit(self.rotate, self.orthogonally_tilted_quadrilateral_plane)
+
+    def TakeDamage(self, amount):
+        self.health -= amount
+        
+      
        
 player = Player(600, 400)
-
 objects = pygame.sprite.Group()
 objects.add(player)
 bullets = pygame.sprite.Group()
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_ESCAPE]: #Added closing the game using ESC
-        pygame.quit()
-        sys.exit()
     
     mouse_pos = pygame.mouse.get_pos()
+    screen.fill(black)
 
     mouse_angle = math.degrees(math.atan2(mouse_pos[0] - player.x, mouse_pos[1] - player.y)) 
     objects.update(dt)
+    bullets.update(dt)
     for i in objects:
         i.draw(mouse_angle)
 
-   
+    for i in bullets:
+        i.draw()
+    
+     #draw the health bar
+    pygame.draw.line(screen, 'red', (10, 10), (10 + player.health, 10), width=5)
+    pygame.draw.line(screen, 'yellow', (10, 20), (10 + player.sun_bar, 20), width=5)
     
     pygame.display.flip()
 
