dbc58c6a7c691a5527de3204f8019a5e7ceceb71
danielfox43 <danielo.rusnaco@gmail.com>
2024-06-13 23:20:01 +0300

    made the camera betta, added gun, gotta fix up the sprite but thats ight, goodnight girl i'll see u tomorrow

--- script.py
+++ script.py
@@ -2,27 +2,49 @@ import sys
 import pygame
 import math
 import time
+from pathfinding.core.grid import Grid
 
 
 
 pygame.init()
 
-size = width, height = 1280, 600
+size = width, height = 540, 300
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, flags=pygame.SCALED)
 
 
 
 clock = pygame.time.Clock()
 dt = 0
+
+def rotate_on_pivot(image, angle, pivot, origin):
+    
+    surf = pygame.transform.rotate(image, angle)
+    ov= pygame.math.Vector2(origin)
+    pv = pygame.math.Vector2(pivot)
+    offset = pivot + (ov - pv).rotate(-angle)
+    rect = surf.get_rect(center = offset)
+    
+    return surf, rect
+"""class Enemy(pygame.sprite.Sprite):
+    def  __init__(self, x, y, image):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(f'{image}.png')
+    def update(self):
+
+    def draw(self):"""
+
+
 class Objects(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.x = 0
         self.y = 0
         self.bgt = pygame.image.load("bg.png")
-        self.bg = pygame.transform.scale(self.bgt, (2500, 2500))
+        self.bg = pygame.transform.scale(self.bgt, (1000, 1000))
 
     def draw(self, screen):
         screen.blit(self.bg, (self.x, self.y))
@@ -39,7 +61,9 @@ class Button(pygame.sprite.Sprite):
         self.height = height
         self.color = color
     def draw(self):
-        pygame.draw.rect(self.surface, self.color, self.rect)        
+         self.surface.fill(self.color)
+         screen.blit(self.surface, self.rect)
+
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, mouse_angle):
@@ -48,7 +72,7 @@ class Bullet(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.mouse_angle = mouse_angle
-        self.speed = 400
+        self.speed = 900
         self.lifetime = 3
         self.rotate = pygame.transform.rotozoom(self.image, mouse_angle, 1)
         self.rect = self.rotate.get_rect(center =(self.x, self.y))
@@ -76,41 +100,63 @@ class Player(pygame.sprite.Sprite):
         self.y = y
         self.x_offset = 0
         self.y_offset = 0
-        self.speed = 400
+        self.speed = 200
         self.health = 100
         self.sun_bar = 100
         self.pressed = False
         self.time = 5
         self.image = pygame.image.load("player.png").convert_alpha()
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.gun = pygame.transform.scale(pygame.image.load("gun.png").convert_alpha(), (16,16))
+        self.gun_rect = self.image.get_rect(center = (self.x, self.y))
         self.last_fired = time.time()
-        self.fire_cooldown = 0.5
+        self.fire_cooldown = 0.4
         self.yy = 0
         self.xx = 0
+        self.flip = False
+        self.limitx = 200
+        self.limiy = 100
+        self.flipping_gun = False
 
     def update(self, dt):
         self.time -= dt
         if pygame.mouse.get_pressed()[0] and time.time() - self.last_fired > self.fire_cooldown:
-            bullet = Bullet(self.x, self.y, mouse_angle)
+            bullet = Bullet(self.gun_rect.x ,self.gun_rect.y, mouse_angle)
             bullets.add(bullet)
             self.last_fired = time.time()
             #self.pressed = True
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.yy += 300 * dt
-        elif keys[pygame.K_s]:
-            self.yy -= 300 * dt
-        if keys[pygame.K_a]:
-            self.xx += 300 * dt
-        elif keys[pygame.K_d]:
-            self.xx -= 300 * dt
 
+
+        if keys[pygame.K_w] and self.y <= self.limiy:
+            self.yy -= self.speed * dt
+        elif keys[pygame.K_s]and self.y >= screen.get_height() - self.limiy:
+            self.yy += self.speed * dt
+        if keys[pygame.K_a] and self.x <= self.limitx:
+            self.xx -= self.speed * dt
+            self.flip = False
+        elif keys[pygame.K_d] and self.x >= screen.get_width()- self.limitx:
+            self.xx += self.speed * dt
+            self.flip = True
+
+        if keys[pygame.K_w]and self.y >= self.limiy:
+            self.y -= self.speed * dt
+        elif keys[pygame.K_s]and self.y <= screen.get_height() - self.limiy:
+            self.y += self.speed * dt
+        if keys[pygame.K_a]and self.x >= self.limitx:
+            self.x -= self.speed * dt
+            self.flip = False
+        elif keys[pygame.K_d]and self.x <= screen.get_width()- self.limitx:
+            self.x += self.speed * dt
+            self.flip = True
+       
+        self.rect = self.image.get_rect(center=(self.x, self.y))
         self.x_offset = self.xx
         self.y_offset = self.yy
-
+        self.gun_rect.x = self.x
+        self.gun_rect.y= self.y 
         self.xx = 0
         self.yy = 0
-        
+        self.flipping_gun = True if(mouse_angle < 0) else False
         
         
         #drains the sun bar every 5 seconds
@@ -125,10 +171,12 @@ class Player(pygame.sprite.Sprite):
 
         return (self.x_offset, self.y_offset)
             
-    def draw(self, mouse_angle):
-        self.rotate = pygame.transform.rotozoom(self.image, mouse_angle, 1)
-        self.orthogonally_tilted_quadrilateral_plane = self.rotate.get_rect(center = (self.x, self.y))
-        screen.blit(self.rotate, self.orthogonally_tilted_quadrilateral_plane)
+    def draw(self):
+        self.rotate = pygame.transform.scale(self.image, (32,32))
+        self.gun_rotate, self.gun_rect = rotate_on_pivot(pygame.transform.flip(self.gun, self.flipping_gun, False), mouse_angle, (self.x, self.y), (self.x, self.y + 30))
+        
+        screen.blit(pygame.transform.flip(self.rotate, self.flip, False), self.rect)
+        screen.blit(self.gun_rotate, self.gun_rect)
 
     def TakeDamage(self, amount):
         self.health -= amount
@@ -148,18 +196,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    #INSIDE OF THE GAME LOOP
-    
 
-#REST OF ITEMS ARE BLIT'D TO SCREEN
     mouse_pos = pygame.mouse.get_pos()
     screen.fill(black)
     mouse_angle = math.degrees(math.atan2(mouse_pos[0] - player.x, mouse_pos[1] - player.y)) 
     objects.update(dt)
     bullets.update(dt)
-
-    for i in ui:
-        i.draw()
+    
+    
 
     player.update(dt)
     for i in objects:
@@ -167,15 +211,18 @@ while True:
         i.y = i.y - player.y_offset
         i.draw(screen)
 
-    player.draw(mouse_angle)
+    player.draw()
     for i in bullets:
         i.x = i.x - player.x_offset
         i.y = i.y - player.y_offset
         i.draw()
     
+    for i in ui:
+        i.draw()
+    
      #draw the health bar
-    pygame.draw.line(screen, 'red', (10, 10), (10 + player.health, 10), width=5)
-    pygame.draw.line(screen, 'yellow', (10, 20), (10 + player.sun_bar, 20), width=5)
+    pygame.draw.line(screen, 'red', (10, 10), (10 + player.health, 10), width=4)
+    pygame.draw.line(screen, 'yellow', (10, 20), (10 + player.sun_bar, 20), width=4)
     
     pygame.display.flip()
 
